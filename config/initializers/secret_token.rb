cfd5ab62380bd729792eb8ef4d09b377833a1bbe
Raphael Villas <raphael.villas@gmail.com>
2010-11-13 11:14:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eatsocial::Application.config.secret_token = 'fb799efcc49cff0dade14224d2e8d7cc1e5a73058029ef85bc18ea1d738d84babf86e643209aee49d1d4d924d47150981465e416f1d9fde2b5b1bc26662471e9'
