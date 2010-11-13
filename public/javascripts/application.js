928333c38ec174605e3cab3c7e711945bda0dd01
Mark Glenn <markglenn@gmail.com>
2010-11-13 17:09:39 -0600

    jquerymobile starting to work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).bind("mobileinit", function(){
+  //apply overrides here
+});
+
 $('.countdown').each(function(){
   $(this).countdown({until: $(this).attr('data-date')})
 });
