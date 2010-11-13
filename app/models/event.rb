a77ce1805d8c25db25a16a971d14d22d2fdd1210
Mark Glenn <markglenn@gmail.com>
2010-11-13 16:54:51 -0600

    Fixed orderby error

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_many :people, :through => :subscriptions, :order => "created_at", :uniq => true
+  has_many :people, :through => :subscriptions, :uniq => true
   has_many :subscriptions
 
   scope :upcoming, where( "time > ?", Time.now )
