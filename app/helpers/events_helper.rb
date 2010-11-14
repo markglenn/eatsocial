2e9db740198d8aeb95ef256154decb65fcfeb41c
Mark Glenn <markglenn@gmail.com>
2010-11-13 23:09:51 -0600

    Added jquery library

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,6 +2,6 @@ module EventsHelper
   def count_description(event)
     count = event.people.count
     
-    "with #{pluralize(count, 'person')}" if count > 1
+    "with #{pluralize(count - 1, 'person')}" if count > 1
   end
 end
